ca34a6677422216bfd0c30485448a13c68ea4e36
devMisa <misaelcosta069@gmail.com>
2025-10-29 16:02:43 -0300

    Teste do repostirio - ver se subiu pro github

---
+++ src/main/java/OrientacaoObjetos/Media.java
@@ -0,0 +1,15 @@
+package OrientacaoObjetos;
+
+public class Media {
+    public static void main(String[] args) {
+
+
+        int n1=20,n2=10;
+        double media;
+
+        media = (n1+n2)/2;
+
+        System.out.println("media:"+ media);
+    }
+}
+
