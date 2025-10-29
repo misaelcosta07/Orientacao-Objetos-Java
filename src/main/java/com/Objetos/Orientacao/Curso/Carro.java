de68f79cf3fc3db24ab4022d34d9ca2b2b080ad0
devMisa <misaelcosta069@gmail.com>
2025-10-29 18:29:00 -0300

    adicionei mais classes ainda tenho um problema com elas

--- src/main/java/com/Objetos/Orientacao/Curso/Carro.java
+++ src/main/java/com/Objetos/Orientacao/Curso/Carro.java
@@ -1,14 +1,13 @@
 package com.Objetos.Orientacao.Curso;
 
-;
-
 public class Carro {
 
     String marca;
-    String nome;
+    String modelo;
     int numpassageiros;
     double capcombustivel; // capacidade de armazenamento de combustivel
     double conscombustivel;  // consumo de combutivel por KM/H
+
 
 }
 
---
+++ src/main/java/com/Objetos/Orientacao/Curso/Lampada.java
@@ -0,0 +1,18 @@
+package com.Objetos.Orientacao.Curso;
+
+public class Lampada {
+
+    String marca;
+    String modelo;
+    double preco;
+    double frequencia; // consumo de energia
+}
+public class TesteLampada {
+
+    lampada led = new lamapada();
+
+    led.marca = "Avant";
+    led.modelo = "led";
+    led.preco = 20;
+    led.frequencia = 60;
+}
