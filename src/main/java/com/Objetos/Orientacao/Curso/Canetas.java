3a388c7024ea5905060310ae730f3245a38af0fe
devMisa <misaelcosta069@gmail.com>
2025-10-30 13:55:05 -0300

    nova class

---
+++ src/main/java/com/Objetos/Orientacao/Curso/Canetas.java
@@ -0,0 +1,26 @@
+package com.Objetos.Orientacao.Curso;
+
+public class Canetas {
+
+    //atributos
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+    boolean tampada;
+
+    // metodos
+
+    void rabiscar() {
+
+    }
+
+    void tampar() {
+
+    }
+
+    void destampar() {
+
+    }
+
+}
---
+++ src/main/java/com/Objetos/Orientacao/Curso/CanetasInstancia.java
@@ -0,0 +1,16 @@
+package com.Objetos.Orientacao.Curso;
+
+public class CanetasInstancia {
+
+    public static void main(String[] args){
+
+        Canetas c1 = new Canetas();
+
+        c1.cor = "Azul";
+        c1.ponta = 0.5;
+        c1.tampada = false;
+
+
+
+    }
+}
