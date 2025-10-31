af52f2ceb39b0c7965b3cf03b1f0a2f781a63009
devMisa <misaelcosta069@gmail.com>
2025-10-31 18:02:48 -0300

    class carro altera com metodos simples

--- src/main/java/com/Objetos/Orientacao/Curso/Carro.java
+++ src/main/java/com/Objetos/Orientacao/Curso/Carro.java
@@ -2,16 +2,37 @@ package com.Objetos.Orientacao.Curso;
 
 public class Carro {
 
-    String marca;
-    String modelo;
-    int numpassageiros;
-    double capcombustivel; // capacidade de armazenamento de combustivel
-    double conscombustivel;  // consumo de combutivel por KM/H
+        String marca;
+        String modelo;
+        int numpassageiros;
+        double capcombustivel; // capacidade de armazenamento de combustivel
+        double conscombustivel;  // consumo de combutivel por KM/H
 
 
+    public static void main(String[] args){
+
+        Carro carro = new Carro();
+
+        carro.marca = "civic";
+        carro.modelo = "honda";
+        carro.numpassageiros = 5;
+        carro.capcombustivel = 50;
+        carro.conscombustivel = 10;
+
+
+    }
     void exibirautonomia(){
 
         System.out.println("A autonomia do carro é:" + capcombustivel * conscombustivel + "KM/h");
     }
+
+    double obterautonomia() {
+        System.out.println("Método obterautonomia foi chamado.");
+        return capcombustivel * conscombustivel;
+    }
 }
+
+
+
+
 
