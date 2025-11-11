e54d11ccf35347b2f3b1678b4b16200b8a56a330
devMisa <misaelcosta069@gmail.com>
2025-11-11 18:05:08 -0300

    tenho que terminar essa class

---
+++ src/main/java/com/Objetos/Orientacao/Curso/Metodos/ContaCorrente.java
@@ -0,0 +1,14 @@
+package com.Objetos.Orientacao.Curso.Metodos;
+
+public class ContaCorrente {
+
+    public int numDaConta;
+    public double saldoConta;
+    public String statusConta;
+    public double limiteEspecial;
+
+   public void sacarDinheiro(double valorDoSaldo){
+
+    }
+}
+
---
+++ src/main/java/com/Objetos/Orientacao/Curso/Metodos/TesteContaCorrente.java
@@ -0,0 +1,15 @@
+package com.Objetos.Orientacao.Curso.Metodos;
+
+public class TesteContaCorrente {
+
+    public static void main(String[] args) {
+        ContaCorrente conta= new ContaCorrente();
+
+        conta.numDaConta= 22310456;
+        conta.saldoConta=500.00;
+        conta.statusConta= "Especial";
+        conta.limiteEspecial = 100.0;
+
+
+    }
+}
